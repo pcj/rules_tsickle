372347bc2dbddc0b876ba16d03552337184c075b
Paul Johnston <pcj@stack.build>
2025-09-28 17:58:55 -0600

    checkpoint: successful initial emit!

--- tools/tsicklecompiler/index.ts
+++ tools/tsicklecompiler/index.ts
@@ -1,197 +1,171 @@
 
 import * as path from 'path';
-import { promises as fs } from 'fs';
+import * as fs from 'fs';
+import { promises as asyncfs } from 'fs';
 import ts from 'typescript';
 import * as tsickle from 'tsickle';
-import { loadTsickleHostConfig, TsickleHostConfig } from './tsicklehost';
-import { CompilerHost, loadTsConfigFromPath } from './compilerhost';
 
 const DEBUG = true;
 
-async function main() {
-    const cwd = process.cwd()
-    const execroot = process.env.JS_BINARY__EXECROOT || '.';
+function getExecRoot(): string {
+    return process.env.JS_BINARY__EXECROOT || '.';
+}
+
+function getBazelBinDir(): string {
+    const binDir = process.env.BAZEL_BINDIR!;
+    return path.join(getExecRoot(), binDir);
+}
+
+function getOutDir(execRoot: string): string {
     const pkgdir = process.env.BAZEL_PACKAGE!;
     const bindir = process.env.BAZEL_BINDIR!;
-    const outdir = path.join(execroot, bindir, pkgdir);
-    // const outdir = path.join(execroot, pkgdir);
-    const tsickleConfigFile = process.env.TSICKLE_CONFIG_FILE!;
-    const tsickleHostConfig = loadTsickleHostConfig(tsickleConfigFile);
-    // const tsConfigFile = `${execroot}/${process.env.TS_CONFIG_FILE!}`;
-    const tsConfigFile = `tsconfig.json`;
+    return path.join(execRoot, bindir, pkgdir);
+}
 
-    const args = process.argv.slice(2);
+async function listFiles(dir: string): Promise<string[]> {
+    return asyncfs.readdir(dir, { recursive: true });
+}
 
-    if (DEBUG) {
-        // console.log('env:', process.env);
-        console.log('pwd:', cwd);
-        console.log('args:', args);
-        console.log('tsickleHostConfig:', tsickleHostConfig);
-        for (const filename of await listFiles(execroot)) {
-            if (filename.indexOf('.aspect_rules_js') >= 0) {
-                continue;
-            }
-            if (filename.indexOf('tsconfig.json') < 0) {
-                continue;
-            }
-            console.log(`{execroot}/${filename}`);
+async function listExecrootFiles() {
+    const execroot = process.env.JS_BINARY__EXECROOT || '.';
+    for (const filename of await listFiles(execroot)) {
+        if (filename.indexOf('.aspect_rules_js') >= 0) {
+            continue;
         }
+        if (filename.indexOf('tsconfig.json') < 0) {
+            continue;
+        }
+        console.log(`{execroot}/${filename}`);
     }
+}
 
-    const inputFiles = args.map(arg => `${execroot}/${arg}`);
-
+function getInputFiles(execRoot: string, args: string[]): string[] {
+    const inputFiles = args.map(arg => `${execRoot}/${arg}`);
     if (inputFiles.length === 0) {
         console.error('Usage: tsicklecompiler <input.ts> [input2.ts] ...');
         process.exit(1);
     }
+    return inputFiles;
+}
 
-    let compilerOptions: ts.CompilerOptions = {
+function getTsCompilerOptions(): ts.CompilerOptions {
+    return {
         target: ts.ScriptTarget.ES2017,
         module: ts.ModuleKind.CommonJS,
-        outDir: outdir,
+        outDir: getBazelBinDir(),
+        rootDir: getExecRoot(),
         declaration: true,
         strict: true
     };
+}
 
-    if (!tsConfigFile) {
-        throw new Error(`tsconfig.json file not set`);
+/**
+ * Determine the lowest-level common parent directory of the given list of files.
+ */
+function getCommonParentDirectory(fileNames: string[]): string {
+    const pathSplitter = /[\/\\]+/;
+    const commonParent = fileNames[0].split(pathSplitter);
+    for (let i = 1; i < fileNames.length; i++) {
+        const thisPath = fileNames[i].split(pathSplitter);
+        let j = 0;
+        while (thisPath[j] === commonParent[j]) {
+            j++;
+        }
+        commonParent.length = j;  // Truncate without copying the array
     }
-
-    if (false) {
-        const { parsedConfig } = loadTsConfigFromPath(tsConfigFile);
-        compilerOptions = parsedConfig.options;
-        compilerOptions.outDir = outdir;
-        compilerOptions.module = ts.ModuleKind.CommonJS;
-        compilerOptions.target = ts.ScriptTarget.ES5;
-    }
-    if (tsickleHostConfig) {
-        await runTsickle(tsickleHostConfig, compilerOptions, inputFiles);
+    if (commonParent.length === 0) {
+        return '/';
     } else {
-        await runVanillaTsc(compilerOptions, inputFiles);
+        return commonParent.join(path.sep);
     }
 }
 
-async function runTsickle(
-    tsickleHostConfig: TsickleHostConfig,
-    compilerOptions: ts.CompilerOptions,
-    inputFiles: string[],
-) {
-    const delegate = ts.createCompilerHost(compilerOptions);
-    const host = new CompilerHost(tsickleHostConfig, compilerOptions, delegate);
+async function main() {
+    const execRoot = getExecRoot();
+    const outDir = getOutDir(execRoot);
+    const args = process.argv.slice(2);
+    const inputFiles = getInputFiles(execRoot, args);
+    // const inputFiles = args;
+    const compilerOptions = getTsCompilerOptions();
 
-    const program = ts.createProgram(inputFiles, compilerOptions, host);
+    if (DEBUG) {
+        const cwd = process.cwd()
+        // console.log('env:', process.env);
+        console.log('pwd:', cwd);
+        console.log('args:', args);
+        console.log('files:', inputFiles);
+        console.log('compilerOptions:', compilerOptions);
+    }
+
+    const result = run(compilerOptions, inputFiles, (filePath: string, contents: string) => {
+        console.log('emitted file:', filePath);
+        fs.mkdirSync(path.dirname(filePath), { recursive: true });
+        fs.writeFileSync(filePath, contents, { encoding: 'utf-8' });
+    });
+
+    if (result.diagnostics.length) {
+        console.error(ts.formatDiagnostics(result.diagnostics, ts.createCompilerHost(compilerOptions)));
+        return 1;
+    }
+
+    // if (settings.externsPath) {
+    //     fs.mkdirSync(path.dirname(settings.externsPath), { recursive: true });
+    //     fs.writeFileSync(
+    //         settings.externsPath,
+    //         tsickle.getGeneratedExterns(result.externs, config.options.rootDir || ''));
+    // }
+
+    return 0;
+
+}
+
+function run(
+    options: ts.CompilerOptions,
+    absoluteFileNames: string[],
+    writeFile: ts.WriteFileCallback,
+): tsickle.EmitResult {
+    // Use absolute paths to determine what files to process since files may be imported using
+    // relative or absolute paths
+    // const absoluteFileNames = fileNames.map(i => path.resolve(i));
+
+    const compilerHost = ts.createCompilerHost(options);
+    const program = ts.createProgram(absoluteFileNames, options, compilerHost);
+    const filesToProcess = new Set(absoluteFileNames);
+    const rootModulePath = options.rootDir || getCommonParentDirectory(absoluteFileNames);
+    const transformerHost: tsickle.TsickleHost = {
+        rootDirsRelative: (f: string) => f,
+        shouldSkipTsickleProcessing: (fileName: string) => {
+            return !filesToProcess.has(path.resolve(fileName));
+        },
+        shouldIgnoreWarningsForPath: (fileName: string) => false,
+        pathToModuleName: (context, fileName) =>
+            tsickle.pathToModuleName(rootModulePath, context, fileName),
+        fileNameToModuleId: (fileName) => path.relative(rootModulePath, fileName),
+        googmodule: true,
+        transformDecorators: true,
+        transformTypesToClosure: true,
+        typeBlackListPaths: new Set(),
+        untyped: false,
+        logWarning: (warning) =>
+            console.error(ts.formatDiagnostics([warning], compilerHost)),
+        options,
+        generateExtraSuppressions: true,
+        // transformDynamicImport: 'nodejs',
+        moduleResolutionHost: compilerHost,
+    };
     const diagnostics = ts.getPreEmitDiagnostics(program);
     if (diagnostics.length > 0) {
-        diagnostics.forEach(diagnostic => {
-            if (diagnostic.file) {
-                const { line, character } = ts.getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start!);
-                const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
-                console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
-            } else {
-                console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'));
-            }
-        });
-        process.exit(12);
+        return {
+            tsMigrationExportsShimFiles: new Map(),
+            diagnostics,
+            modulesManifest: new tsickle.ModulesManifest(),
+            externs: {},
+            emitSkipped: true,
+            emittedFiles: [],
+            // fileSummaries: new Map(),
+        };
     }
-
-    let targetSourceFile: ts.SourceFile | undefined = undefined;
-    for (const sf of program.getSourceFiles()) {
-        console.log('program source file:', sf.fileName);
-        targetSourceFile = sf;
-    }
-
-    const emittedFiles: string[] = [];
-    const result: tsickle.EmitResult = tsickle.emit(program, host, (fileName, data) => {
-        emittedFiles.push(fileName);
-        host.writeFile(fileName, data, false);
-    }, targetSourceFile);
-
-    if (result.diagnostics.length > 0) {
-        diagnostics.forEach(diagnostic => {
-            if (diagnostic.file) {
-                const { line, character } = ts.getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start!);
-                const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
-                console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
-            } else {
-                console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'));
-            }
-        });
-    }
-    // const emittedFiles: string[] = [];
-    // const emitResult = program.emit(undefined, (fileName, data) => {
-    //     emittedFiles.push(fileName);
-    //     host.writeFile(fileName, data, false);
-    // });
-
-    // const diagnostics = ts
-    //     .getPreEmitDiagnostics(program)
-    //     .concat(emitResult.diagnostics);
-
-    // diagnostics.forEach(diagnostic => {
-    //     if (diagnostic.file) {
-    //         const { line, character } = ts.getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start!);
-    //         const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
-    //         console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
-    //     } else {
-    //         console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'));
-    //     }
-    // });
-
-    if (DEBUG) {
-        if (emittedFiles.length > 0) {
-            console.log('Emitted files:');
-            emittedFiles.forEach(file => console.log(`  ${file}`));
-        } else {
-            console.log('No files were emitted');
-        }
-    }
-
-    // const exitCode = emitResult.emitSkipped ? 1 : 0;
-    // process.exit(exitCode);
-}
-
-async function runVanillaTsc(
-    compilerOptions: ts.CompilerOptions,
-    inputFiles: string[],
-) {
-    const host = ts.createCompilerHost(compilerOptions);
-    const program = ts.createProgram(inputFiles, compilerOptions, host);
-
-    const emittedFiles: string[] = [];
-    const emitResult = program.emit(undefined, (fileName, data) => {
-        emittedFiles.push(fileName);
-        host.writeFile(fileName, data, false);
-    });
-
-    const diagnostics = ts
-        .getPreEmitDiagnostics(program)
-        .concat(emitResult.diagnostics);
-
-    diagnostics.forEach(diagnostic => {
-        if (diagnostic.file) {
-            const { line, character } = ts.getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start!);
-            const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
-            console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
-        } else {
-            console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'));
-        }
-    });
-
-    if (DEBUG) {
-        if (emittedFiles.length > 0) {
-            console.log('Emitted files:');
-            emittedFiles.forEach(file => console.log(`  ${file}`));
-        } else {
-            console.log('No files were emitted');
-        }
-    }
-
-    const exitCode = emitResult.emitSkipped ? 1 : 0;
-    process.exit(exitCode);
-}
-
-async function listFiles(dir: string): Promise<string[]> {
-    return fs.readdir(dir, { recursive: true });
+    return tsickle.emit(program, transformerHost, writeFile);
 }
 
 void main()
